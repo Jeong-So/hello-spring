893ec0cf2014f06af14972cba6ce505e1280674f
Jeong-So <83945245+Jeong-So@users.noreply.github.com>
2023-01-30 22:19:52 +0900

    (20230130) 회원 관리 예제 - 회원 서비스 테스트

--- src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/repository/MemoryMemberRepositoryTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.*;
 
 public class MemoryMemberRepositoryTest {
 
-    MemoryMemberRepository repository = new MemoryMemberRepository();
+    MemoryMemberRepository memberRepository = new MemoryMemberRepository();
 
 //  테스트는 서로 순서와 상관없이 (=의존관계 없이) 설계 필요, 하나의 테스트가 끝날때마다 레포지토리 클리어 필요
 //  테스트가 끝날 때마다 repository를 깔끔하게 지워주는 코드 필요
@@ -20,7 +20,7 @@ public class MemoryMemberRepositoryTest {
 //  save 끝나고 동작 findByName 끝나고 동작
     @AfterEach
     public void afterEach() {
-        repository.clearStore();
+        memberRepository.clearStore();
     }
 
     @Test
@@ -28,10 +28,10 @@ public class MemoryMemberRepositoryTest {
         Member member = new Member();
         member.setName("spring");
 
-        repository.save(member);
+        memberRepository.save(member);
 
         // Optional에서 값을 꺼낼 때는 .get() 사용하여 꺼냄
-        Member result = repository.findById(member.getId()).get();
+        Member result = memberRepository.findById(member.getId()).get();
 //        System.out.println("result = " + (result == member));
 
         Assertions.assertEquals(result, member);
@@ -42,13 +42,13 @@ public class MemoryMemberRepositoryTest {
     public void findByName() {
         Member member1 = new Member();
         member1.setName("spring1");
-        repository.save(member1);
+        memberRepository.save(member1);
 
         Member member2 = new Member();
         member2.setName("spring2");
-        repository.save(member2);
+        memberRepository.save(member2);
 
-        Member result = repository.findByName("spring1").get();
+        Member result = memberRepository.findByName("spring1").get();
 
         assertThat(result).isEqualTo(member1);
 
@@ -58,13 +58,13 @@ public class MemoryMemberRepositoryTest {
     public void findAll() {
         Member member1 = new Member();
         member1.setName("spring1");
-        repository.save(member1);
+        memberRepository.save(member1);
 
         Member member2 = new Member();
         member2.setName("spring2");
-        repository.save(member2);
+        memberRepository.save(member2);
 
-        List<Member> result = repository.findAll();
+        List<Member> result = memberRepository.findAll();
 
         assertThat(result.size()).isEqualTo(2);
 
---
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -0,0 +1,87 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemoryMemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+
+    MemberService memberService = new MemberService();
+    MemoryMemberRepository memberRepository;
+
+    // memberServiceTest와 memberService가 다른 memberRepository를 쓰지 않기위해
+    // 각 테스트 코드 실행 전 memberService에 memberRepository 주입 (DI)
+    @BeforeEach
+    public void beforeEach() {
+        memberRepository = new MemoryMemberRepository();
+        memberService = new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    public void afterEach() {
+        memberRepository.clearStore();
+    }
+
+
+    @Test
+    void join() {
+        // given
+        Member member = new Member();
+        member.setName("hello");
+
+        // when
+        Long saveId = memberService.join(member);
+
+        // then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+
+    }
+
+    @Test
+    void 중복_회원_예외() {
+        // given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        // when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+//      () -> memberService.join(member2) 로직을 실행할 것인데 IllegalStateException 예외가 터져야 함
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+
+
+//        try {
+//            memberService.join(member2);
+//            fail();
+//        } catch (IllegalStateException e) {
+//            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다. 1234");
+//        }
+
+        // then
+
+    }
+
+    @Test
+    void findMembers() {
+
+    }
+
+    @Test
+    void findOne() {
+
+    }
+
+}
